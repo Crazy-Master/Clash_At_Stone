9445de5ca1d870aeadc1469d5a022c932f37f788
Crazy_Master <gadaychenkov99@mail.ru>
2023-03-15 22:56:27 +0300

    spawn walls

--- Assets/Scripts/SpawnStone.cs
+++ Assets/Scripts/SpawnStone.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,43 @@ using Random = UnityEngine.Random;
 
 public class SpawnStone : MonoBehaviour
 {
-    [ContextMenu("StoneSpawn")]
-
-    public void StoneSpawn()
+    private int[] _horizontallyStone = new int [2] ;
+    [SerializeField] private GameObject _stone;
+    
+    private void Update()
     {
-        var i = Random.Range(-3, 4);
-        int y = 0;
+        if (Input.GetMouseButtonDown(1)) StoneSpawn(_stone);
+    }
+
+    public void StoneSpawn(GameObject stone)
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            HorizontallyStone();
+            Instantiate(stone, new Vector3(_horizontallyStone[0],0,i), Quaternion.identity);
+            Instantiate(stone, new Vector3(_horizontallyStone[0],0,i+7), Quaternion.identity);
+            Instantiate(stone, new Vector3(_horizontallyStone[1],0,i), Quaternion.identity);
+            Instantiate(stone, new Vector3(_horizontallyStone[1],0,i+7), Quaternion.identity);
+        }
+        
+    }
+
+    private void HorizontallyStone()
+    {
+        int  firstStone = Random.Range(0, 8);
+        int secondStone = 0;
         int distance = 0;
         while (distance < 2 || distance > 4)
         {
-            Debug.Log("зашёл");
-           y = Random.Range(-3, 4);
-           distance = i - y;
-           if (distance < 0)
-           {
-               distance = distance * -1;
-           }
+            secondStone = Random.Range(0, 8);
+            distance = firstStone - secondStone;
+            if (distance < 0)
+            {
+                distance = distance * -1;
+            }
         }
-        Debug.Log("distance_" + distance);
+
+        _horizontallyStone[0] = firstStone;
+        _horizontallyStone[1] = secondStone;
     }
 }
